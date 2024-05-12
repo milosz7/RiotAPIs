1e504be0088f1c7287aa48b9d8754788da63ddc5
Jeremi Torój <jeremi.toroj@icloud.com>
2024-05-12 14:56:39 +0200

    fix constructor vars changed to use .env vars

--- database/DatabaseConnection.py
+++ database/DatabaseConnection.py
@@ -5,7 +5,7 @@ import os
 
 
 class DatabaseConnection:
-    def __init__(self, dotenv_path: str, timeout: int=10):
+    def __init__(self, dotenv_path: str, timeout: int = 10):
         load_dotenv(dotenv_path=dotenv_path)
         self.connection = pymysql.connect(
             charset="utf8mb4",
@@ -15,7 +15,7 @@ class DatabaseConnection:
             host=os.getenv('HOST'),
             password=os.getenv('PASS'),
             read_timeout=timeout,
-            port=11092,
-            user="avnadmin",
+            port=int(os.getenv('PORT')),
+            user=os.getenv('USER'),
             write_timeout=timeout,
         )
