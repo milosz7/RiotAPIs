36c571c9466fa43baba37a61ea95395dc0dca022
Jeremi Torój <jeremi.toroj@icloud.com>
2024-05-14 12:12:28 +0200

    add DatabaseConnection.py class champion_bans table CRUD

--- database/DatabaseConnection.py
+++ database/DatabaseConnection.py
@@ -28,7 +28,7 @@ class DatabaseConnection:
             cursor.execute(query, args)
             return cursor.fetchall()
 
-    def display_champions(self):
+    def get_champions(self):
         return self.query("SELECT * FROM champions")
 
     def add_champion(self, champ_id: int, name: str):
@@ -41,4 +41,26 @@ class DatabaseConnection:
 
     def update_champion(self, champ_id: int, name: str):
         self.query("UPDATE champions SET champion_name = %s WHERE champion_id = %s", (name, champ_id))
+        self.connection.commit()
+
+    def get_champion_bans(self):
+        return self.query("SELECT * FROM champion_bans")
+
+    def add_champion_bans(self, match_id: int, ban_1: int, ban_2: int, ban_3: int, ban_4: int, ban_5: int, ban_6: int,
+                          ban_7: int, ban_8: int, ban_9: int, ban_10: int):
+        self.query("INSERT INTO champion_bans (match_id, ban_1, ban_2, ban_3, ban_4, ban_5,"
+                   " ban_6, ban_7, ban_8, ban_9, ban_10)"
+                   " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                   (match_id, ban_1, ban_2, ban_3, ban_4, ban_5, ban_6, ban_7, ban_8, ban_9, ban_10))
+        self.connection.commit()
+
+    def delete_champion_bans(self, match_id: int):
+        self.query("DELETE FROM champion_bans WHERE match_id = %s", (match_id,))
+        self.connection.commit()
+
+    def update_champion_bans(self, match_id: int, ban_1: int, ban_2: int, ban_3: int, ban_4: int, ban_5: int,
+                             ban_6: int, ban_7: int, ban_8: int, ban_9: int, ban_10: int):
+        self.query("UPDATE champion_bans SET ban_1 = %s, ban_2 = %s, ban_3 = %s, ban_4 = %s, ban_5 = %s,"
+                   " ban_6 = %s, ban_7 = %s, ban_8 = %s, ban_9 = %s, ban_10 = %s WHERE match_id = %s",
+                   (ban_1, ban_2, ban_3, ban_4, ban_5, ban_6, ban_7, ban_8, ban_9, ban_10, match_id))
         self.connection.commit()
