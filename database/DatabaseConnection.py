1b3f1855e873c6b492950f01dce2103a5060da6b
Jeremi Torój <jeremi.toroj@icloud.com>
2024-05-14 11:59:10 +0200

    add DatabaseConnection.py class champion table CRUD

--- database/DatabaseConnection.py
+++ database/DatabaseConnection.py
@@ -19,3 +19,26 @@ class DatabaseConnection:
             user=os.getenv('USER'),
             write_timeout=timeout,
         )
+
+    def __del__(self):
+        self.connection.close()
+
+    def query(self, query: str, args: tuple = ()):
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, args)
+            return cursor.fetchall()
+
+    def display_champions(self):
+        return self.query("SELECT * FROM champions")
+
+    def add_champion(self, champ_id: int, name: str):
+        self.query("INSERT INTO champions (champion_id, champion_name) VALUES (%s, %s)", (champ_id, name))
+        self.connection.commit()
+
+    def delete_champion(self, champ_id: int):
+        self.query("DELETE FROM champions WHERE champion_id = %s", (champ_id,))
+        self.connection.commit()
+
+    def update_champion(self, champ_id: int, name: str):
+        self.query("UPDATE champions SET champion_name = %s WHERE champion_id = %s", (name, champ_id))
+        self.connection.commit()
