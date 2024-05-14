2ec47b270836d366f257c624298fb0ad62a7baf5
Jeremi Torój <jeremi.toroj@icloud.com>
2024-05-14 13:27:40 +0200

    add DatabaseConnection.py class CRUD for match_data and player_data + 2 functions to write

--- database/DatabaseConnection.py
+++ database/DatabaseConnection.py
@@ -28,6 +28,12 @@ class DatabaseConnection:
             cursor.execute(query, args)
             return cursor.fetchall()
 
+    def generate_add_query(self):
+        pass
+
+    def generate_update_query(self):
+        pass
+
     def get_champions(self):
         return self.query("SELECT * FROM champions")
 
@@ -63,4 +69,65 @@ class DatabaseConnection:
         self.query("UPDATE champion_bans SET ban_1 = %s, ban_2 = %s, ban_3 = %s, ban_4 = %s, ban_5 = %s,"
                    " ban_6 = %s, ban_7 = %s, ban_8 = %s, ban_9 = %s, ban_10 = %s WHERE match_id = %s",
                    (ban_1, ban_2, ban_3, ban_4, ban_5, ban_6, ban_7, ban_8, ban_9, ban_10, match_id))
+        self.connection.commit()
+
+    def get_matches(self):
+        return self.query("SELECT * FROM match_data")
+
+    def add_match(self, match_id: int, game_duration: int, win: str, first_drake: str, dragon_kills: int,
+                  first_baron: str, surrender: int):
+        self.query("INSERT INTO match_data (match_id, game_duration, win,"
+                   " first_drake, dragon_kills, first_baron, surrender)"
+                   " VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                   (match_id, game_duration, win, first_drake, dragon_kills, first_baron, surrender))
+        self.connection.commit()
+
+    def delete_match(self, match_id: int):
+        self.query("DELETE FROM match_data WHERE match_id = %s", (match_id,))
+        self.connection.commit()
+
+    def update_match(self, match_id: int, game_duration: int, win: str, first_drake: str, dragon_kills: int,
+                     first_baron: str, surrender: str):
+        self.query("UPDATE match_data SET game_duration = %s, win = %s, first_drake = %s, dragon_kills = %s,"
+                   " first_baron = %s, surrender = %s WHERE match_id = %s",
+                   (game_duration, win, first_drake, dragon_kills, first_baron, surrender, match_id))
+        self.connection.commit()
+
+    def get_player_data(self):
+        return self.query("SELECT * FROM player_data")
+
+    def add_player_data(self, match_id: int, team_id: int, lane: str, rank: str, division: int, champion_id: int,
+                        first_blood: int, kills: int, deaths: int, assists: int, dmg_per_min: int,
+                        dmg_taken_per_min: int, total_time_dead: int, gold_per_min: int, wards_placed: int,
+                        sight_wards_bought: int, wards_destroyed: int, vision_score_per_min: int, dmg_to_towers: int,
+                        cs_per_min: int, missing_pings: int):
+        self.query("INSERT INTO player_data (match_id, team_id, lane, `rank`, division, champion_id,"
+                   " first_blood, kills, deaths, assists, dmg_per_min, dmg_taken_per_min, total_time_dead,"
+                   " gold_per_min, wards_placed, sight_wards_bought, wards_destroyed, vision_score_per_min,"
+                   " dmg_to_towers, cs_per_min, missing_pings)"
+                   " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                   (match_id, team_id, lane, rank, division, champion_id, first_blood, kills, deaths,
+                    assists, dmg_per_min, dmg_taken_per_min, total_time_dead, gold_per_min, wards_placed,
+                    sight_wards_bought, wards_destroyed, vision_score_per_min, dmg_to_towers, cs_per_min,
+                    missing_pings))
+        self.connection.commit()
+
+    def delete_player_data(self, match_id: int):
+        self.query("DELETE FROM player_data WHERE match_id = %s", (match_id,))
+        self.connection.commit()
+
+    def update_player_data(self, match_id: int, team_id: int, lane: str, rank: str, division: int, champion_id: int,
+                           first_blood: int, kills: int, deaths: int, assists: int, dmg_per_min: int,
+                           dmg_taken_per_min: int, total_time_dead: int, gold_per_min: int, wards_placed: int,
+                           sight_wards_bought: int, wards_destroyed: int, vision_score_per_min: int, dmg_to_towers: int,
+                           cs_per_min: int, missing_pings: int):
+
+        self.query("UPDATE player_data SET team_id = %s, lane = %s, `rank` = %s, division = %s, champion_id = %s,"
+                   " first_blood = %s, kills = %s, deaths = %s, assists = %s, dmg_per_min = %s,"
+                   " dmg_taken_per_min = %s, total_time_dead = %s, gold_per_min = %s, wards_placed = %s,"
+                   " sight_wards_bought = %s, wards_destroyed = %s, vision_score_per_min = %s,"
+                   " dmg_to_towers = %s, cs_per_min = %s, missing_pings = %s WHERE match_id = %s",
+                   (team_id, lane, rank, division, champion_id, first_blood, kills, deaths, assists, dmg_per_min,
+                    dmg_taken_per_min, total_time_dead, gold_per_min, wards_placed, sight_wards_bought, wards_destroyed,
+                    vision_score_per_min, dmg_to_towers, cs_per_min, missing_pings, match_id))
         self.connection.commit()
